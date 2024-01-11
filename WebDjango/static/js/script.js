a4a5a5da7d330a619340859f511f36c729f214db
Artemii <angap4@gmail.com>
2024-01-11 18:14:19 +1000

    yes

--- WebDjango/static/js/script.js
+++ WebDjango/static/js/script.js
@@ -39,34 +39,7 @@ function scrollToHeight(height) {
         behavior: 'smooth' //Добавляет плавность прокрутке
     });
 }
-function set_size(){
-    let flag1 = false;
-    let flag2 = false;
-    if(document.getElementById('quiz-block1').style.display == 'none'){
-        document.getElementById('quiz-block1').style.display = 'block';
-        flag1 = true;
-    }
-    if(document.getElementById('quiz-block3').style.display == 'none'){
-        document.getElementById('quiz-block3').style.display = 'block';
-        flag2 = true;
-    }
-    let my_height = document.getElementById('sub-block3').clientHeight;
-    let my_width = document.getElementById('sub-block3').clientWidth;
 
-    document.getElementById('sub-block1').style.height = my_height.toString() + 'px';
-    document.getElementById('sub-block2').style.height = my_height.toString() + 'px';
-
-    document.getElementById('quiz-block3').style.height = document.getElementById('quiz-block1').clientHeight.toString() + 'px';
-
-    document.getElementById('sub-block1').style.width = my_width.toString() + 'px';
-    document.getElementById('sub-block2').style.width = my_width.toString() + 'px';
-    if(flag1){
-        document.getElementById('quiz-block1').style.display = 'none';
-    }
-    if(flag2){
-        document.getElementById('quiz-block3').style.display = 'none';
-    }
-}
 function get_result() {
     // получение данных
     let data = new FormData();
